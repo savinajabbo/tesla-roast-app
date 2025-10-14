500fb373a4ddd4452a2c6b66e9ba8cbcf26558e5
Savina Jabbo <122177504+savinajabbo@users.noreply.github.com>
2025-10-13 17:06:21 -0700

    created sign in button
    adjusted other files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        ".pages/**/*.{js,ts, tsx, jsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
