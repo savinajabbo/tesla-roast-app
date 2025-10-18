813102b74d33b653f51832ba0f0fca5df3858cf3
Savina Jabbo <122177504+savinajabbo@users.noreply.github.com>
2025-10-18 12:55:09 -0700

    updated route with credentials

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -5,16 +5,21 @@ export async function GET(req: Request) {
     const code = searchParams.get("code");
 
     if (!code) {
-        return NextResponse.json({ error: "Missing code from Tesla" }, { status: 400 });
+        return NextResponse.json({ error: "missing code from tesla" }, { status: 400 });
     }
 
     const tokenURL = "https://auth.tesla.com/oauth2/v3/token";
 
+    const clientId = "0781fc62-6073-459e-84ca-ab343e1699fa";
+    const clientSecret = process.env.TESLA_CLIENT_SECRET;
+    const redirectUri = "https://tesla-roast-app.vercel.app/api/auth/callback";
+
     const body = {
         grant_type: "authorization_code",
-        client_id: "ownerapi",
+        client_id: clientId,
+        clientSecret: clientSecret,
         code,
-        redirect_uri: "https://tesla-roast-app.vercel.app/api/auth/callback",
+        redirect_uri: redirectUri,
     };
 
     const response = await fetch(tokenURL, {
@@ -25,7 +30,7 @@ export async function GET(req: Request) {
         body: JSON.stringify(body),
     });
 
-    const date = await response.json();
+    const data = await response.json();
 
-    return NextResponse.json(date);
+    return NextResponse.json(data);
 }
